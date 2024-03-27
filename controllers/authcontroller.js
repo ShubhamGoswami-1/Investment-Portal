bafe4e8fcc5c3cccf15bdf4f56b94cdea32a0ef5
Shubham Goswami <shubham24976@gmail.com>
2024-03-27 11:25:09 +0530

    hhtpOnly false in SSO Login

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -281,7 +281,7 @@ exports.OauthJWTtoken = asyncErrorHandler(async(req, res, next) => {
  
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        httpOnly: true
+        httpOnly: false
     };
  
     res.cookie('jwt', token, cookieOptions);
